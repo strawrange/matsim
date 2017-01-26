af36b67112908dbf979131e7eb1020626b61923f
lh_beijing <lh_beijing@10.0.146.24>
2017-01-26 17:26:47 +0100

    add passenger to transport Mode

--- matsim/src/main/java/org/matsim/api/core/v01/TransportMode.java
+++ matsim/src/main/java/org/matsim/api/core/v01/TransportMode.java
@@ -37,6 +37,7 @@ public final class TransportMode {
 	public static final String walk = "walk";
 	public static final String transit_walk = "transit_walk";
 	public static final String ride_share_driver = "ride_share_driver";
+	public static final String ride_share_passenger = "ride_share_passenger";
 	
 	
 	public static final String access_walk = "access_walk" ; // "bushwhacking" from facility to network
