b1fad4621a0acb3f2ce0485871aaccfb00f49d36
lh_beijing <lh_beijing@public-docking-hix-7304.ethz.ch>
2017-01-30 17:15:25 +0100

    add ridesharestay score

---
+++ contribs/dvrp/src/main/java/rideSharing/RideShareScoringFunctionFactory.java
@@ -0,0 +1,82 @@
+package rideSharing;
+
+import java.util.List;
+
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.population.Activity;
+import org.matsim.api.core.v01.population.Leg;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.api.core.v01.population.PlanElement;
+import org.matsim.core.scoring.ScoringFunction;
+import org.matsim.core.scoring.ScoringFunctionFactory;
+import org.matsim.core.scoring.SumScoringFunction;
+import org.matsim.core.scoring.functions.CharyparNagelActivityScoring;
+import org.matsim.core.scoring.functions.CharyparNagelAgentStuckScoring;
+import org.matsim.core.scoring.functions.CharyparNagelLegScoring;
+import org.matsim.core.scoring.functions.CharyparNagelMoneyScoring;
+import org.matsim.core.scoring.functions.CharyparNagelScoringParameters;
+
+public class RideShareScoringFunctionFactory implements ScoringFunctionFactory {
+
+	Scenario scenario;
+	
+	void ScoringFuctionFactory(){
+	}
+	
+	void ScoringFuctionFacory(Scenario scenario){
+		this.scenario = scenario;
+	}
+	
+	@Override
+	public ScoringFunction createNewScoringFunction(Person person) {
+				
+		SumScoringFunction sumScoringFunction = new SumScoringFunction();
+		
+		final CharyparNagelScoringParameters params =
+				new CharyparNagelScoringParameters.Builder(scenario, person.getId()).build();
+		sumScoringFunction.addScoringFunction(new CharyparNagelActivityScoring(params));
+		sumScoringFunction.addScoringFunction(new CharyparNagelLegScoring(params, scenario.getNetwork()));
+		sumScoringFunction.addScoringFunction(new CharyparNagelMoneyScoring(params));
+		sumScoringFunction.addScoringFunction(new CharyparNagelAgentStuckScoring(params));
+		
+		boolean isRideShareDriver = false;
+		
+		List<PlanElement> plans = person.getSelectedPlan().getPlanElements();
+		for(PlanElement plan: plans){
+			if(plan instanceof Leg){
+				if(((Leg)plan).getMode().equals(Run.MODE_DRIVER)){
+					isRideShareDriver = true;
+					break;
+					}
+			}
+		}
+		
+		
+		if (isRideShareDriver) {
+			
+			sumScoringFunction.addScoringFunction(new SumScoringFunction.LegScoring() {
+				
+				private double score;
+				
+				@Override
+				public double getScore() {
+					return this.score;
+				}
+				
+				@Override
+				public void finish() {					
+				}
+				
+				@Override
+				public void handleLeg(Leg leg) {
+					// TODO Auto-generated method stub
+					
+				}
+			});
+		}
+		
+		
+		return null;
+	}
+
+}
